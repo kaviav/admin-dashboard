dd93bef2d6b0d0bfdca1bd69a152eca8c96e0479
Whale77777 <kavyasathyanathan@gmail.com>
2023-10-09 14:53:39 +0530

    added

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5000/";
+const BASE_URL = "https://ecommerce-backend-j4t4.onrender.com/";
 // const token =
 //   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0ZmJjZWVjMTU3YzA5ZGUzOWIzOTQ3NyIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY5NDk0MzE0NCwiZXhwIjoxNjk1MjAyMzQ0fQ.bcDeCtVSq8oZ43hcBYqGsy2DusEuam-RTP0uPvHXuMQ";
 
