1704c695dd0e52cbb6cad902e3330bc300127ff1
Whale77777 <kavyasathyanathan@gmail.com>
2023-10-09 17:39:56 +0530

    added

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -4,13 +4,12 @@ const BASE_URL = "https://ecommerce-backend-0buv.onrender.com/";
 // const token =
 //   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0ZmJjZWVjMTU3YzA5ZGUzOWIzOTQ3NyIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY5NDk0MzE0NCwiZXhwIjoxNjk1MjAyMzQ0fQ.bcDeCtVSq8oZ43hcBYqGsy2DusEuam-RTP0uPvHXuMQ";
 
-const token = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user)
-  .currentUser.accessToken;
-
-// console.log(
-//   JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser
-//     .accessToken
-// );
+const user = JSON.parse(localStorage.getItem("persist:root"))?.user;
+// console.log(user);
+const currentUser = user && JSON.parse(user)?.currentUser;
+// console.log(currentUser);
+const token = currentUser?.accessToken;
+console.log(token);
 
 export const publicRequest = axios.create({
   baseURL: BASE_URL,
